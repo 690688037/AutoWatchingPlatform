177b7e5c6d4574974013da068a0b5f780a4d07af
EDO <37139897+690688037@users.noreply.github.com>
2022-05-05 09:24:47 +0800

    Update SharesApi.cs

--- Model/Common/SharesApi.cs
+++ Model/Common/SharesApi.cs
@@ -10,11 +10,5 @@ namespace AutoWatchingPlatform.Model
     {
         public const string getSharesDataByCode = "http://qt.gtimg.cn/q={0}";
         public const string getAllShares = "http://87.push2.eastmoney.com/api/qt/clist/get?pn=1&pz=10000&po=1&np=1&fltt=2&invt=2&fid=f3&fs=m:0+t:6,m:0+t:80,m:1+t:2,m:1+t:23&fields=f12,f14%27";
-
-        //public static string urlHeader = "http://localhost:8080";
-        public const string urlShares = "/sharesdata/shares";
-        public const string urlWatching = "/sharesdata/watching";
-        public const string urlPrice = "/sharesdata/price";
-        public const string urlDatabaseConfiguration = "/sharesdata/databaseconfiguration";
     }
 }
